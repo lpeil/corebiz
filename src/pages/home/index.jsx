c8ec53d1c7ccfeb476c9b119d7f0187b3ee7b7bb
lpeil <luan@peil.dev>
2021-04-28 17:44:36 -0300

    feat(home): load products

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { Carousel } from '../../components';
+import { Grid } from '@material-ui/core';
+import { Carousel, Product } from '../../components';
 
-const HomePage = () => <div><Carousel /></div>;
+import { apiGetProducts } from '../../services/products';
+import { setProducts } from '../../store/modules/products/actions';
+
+const HomePage = () => {
+  const productsStore = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    apiGetProducts().then((data) => {
+      dispatch(setProducts(data));
+    });
+  }, []);
+
+  return (
+    <div>
+      <Carousel />
+      <Grid container justify="center">
+        <Grid item container xs={9} direction="column">
+          <Grid item>
+            <h1>Mais Vendidos</h1>
+          </Grid>
+          <Grid item container direction="row" justify="space-around">
+            {productsStore.map((product) => (
+              <Grid item key={product.productId}>
+                <Product product={product} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default HomePage;
