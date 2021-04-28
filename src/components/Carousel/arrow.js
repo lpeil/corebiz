7fb3a23cc4ed007cc4ec7d42ce04063233a08d4c
lpeil <luan@peil.dev>
2021-04-28 19:02:00 -0300

    feat: create products slider

---
+++ src/components/Carousel/arrow.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { IconButton } from '@material-ui/core';
+
+import { NavigateNext, NavigateBefore } from '@material-ui/icons';
+
+import './style.css';
+
+const NextArrow = ({ onClick, direction }) => (
+  <IconButton className={`arrow ${direction}`} onClick={onClick} component="span">
+    {
+      direction === 'prev'
+        ? <NavigateBefore style={{ fontSize: 30 }} />
+        : <NavigateNext style={{ fontSize: 30 }} />
+    }
+  </IconButton>
+);
+
+NextArrow.propTypes = {
+  direction: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+NextArrow.defaultProps = {
+  onClick: () => {},
+};
+
+export default NextArrow;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,10 +2,14 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { Grid } from '@material-ui/core';
+import Slider from 'react-slick';
 import { Carousel, Product } from '../../components';
+import Arrow from '../../components/Carousel/arrow';
 
 import { apiGetProducts } from '../../services/products';
 import { setProducts } from '../../store/modules/products/actions';
+
+import './style.css';
 
 const HomePage = () => {
   const productsStore = useSelector((state) => state.products);
@@ -17,6 +21,15 @@ const HomePage = () => {
     });
   }, []);
 
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    nextArrow: <Arrow direction="next" />,
+    prevArrow: <Arrow direction="prev" />,
+  };
+
   return (
     <div>
       <Carousel />
@@ -25,12 +38,12 @@ const HomePage = () => {
           <Grid item>
             <h1>Mais Vendidos</h1>
           </Grid>
-          <Grid item container direction="row" justify="space-around">
-            {productsStore.map((product) => (
-              <Grid item key={product.productId}>
-                <Product product={product} />
-              </Grid>
-            ))}
+          <Grid item xs={12}>
+            <Slider {...sliderSettings} className="products-slider">
+              {productsStore.map((product) => (
+                <Product key={product.productId} product={product} />
+              ))}
+            </Slider>
           </Grid>
         </Grid>
       </Grid>
