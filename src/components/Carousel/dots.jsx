5f4646880e8d00e84a64b51cd9a2153bc52732c3
lpeil <luan@peil.dev>
2021-04-29 15:15:17 -0300

    fix(products): fix sizes on mobile

--- src/components/Carousel/dots.jsx
+++ src/components/Carousel/dots.jsx
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-const Dots = ({ dots }) => (<div className="custom-dots">{dots}</div>);
+const Dots = ({ dots, mobile }) => (
+  <div className={`custom-dots ${mobile ? 'mobile' : ''}`}>
+    {dots}
+  </div>
+);
 
 Dots.propTypes = {
   dots: PropTypes.node.isRequired,
+  mobile: PropTypes.bool,
+};
+
+Dots.defaultProps = {
+  mobile: false,
 };
 
 export default Dots;
--- src/pages/home/components/products.jsx
+++ src/pages/home/components/products.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import Slider from 'react-slick';
 import { Product } from '../../../components';
 import Arrow from '../../../components/Carousel/arrow';
+import Dots from '../../../components/Carousel/dots';
 
 import '../style.css';
 
@@ -17,6 +18,25 @@ const HomeProducts = () => {
     slidesToScroll: 4,
     nextArrow: <Arrow direction="next" />,
     prevArrow: <Arrow direction="prev" />,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false,
+          dots: true,
+          appendDots: (dots) => <Dots dots={dots} mobile />
+        }
+      }
+    ]
   };
 
   return (
