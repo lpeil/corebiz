6ef1c146a71d761e3ad194b14d1d5dcb28d0e5ee
lpeil <luan@peil.dev>
2021-04-29 16:43:39 -0300

    fix: make navbar resizable

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { Debounce } from 'react-throttle';
 import { useHistory, Link } from 'react-router-dom';
@@ -7,12 +8,34 @@ import {
   Grid, Input, InputAdornment, Button, Badge, Hidden,
 } from '@material-ui/core';
 import {
-  Search as SearchIcon, PersonOutline as PersonIcon, ShoppingCartOutlined as CartIcon,
+  Search as SearchIcon,
+  PersonOutline as PersonIcon,
+  ShoppingCartOutlined as CartIcon,
+  Menu as MenuIcon,
 } from '@material-ui/icons';
 
 import CorebizLogo from '../../assets/logo.svg';
 
 import './style.css';
+
+const SearchInput = ({ onChange }) => (
+  <Debounce time="1000" handler="onChange">
+    <Input
+      placeholder="O que está procurando?"
+      fullWidth
+      onChange={onChange}
+      endAdornment={(
+        <InputAdornment position="end">
+          <SearchIcon />
+        </InputAdornment>
+      )}
+    />
+  </Debounce>
+);
+
+SearchInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
 
 const Navbar = () => {
   const cartStore = useSelector((state) => state.cart);
@@ -30,37 +53,40 @@ const Navbar = () => {
 
   return (
     <Grid container justify="center" className="navbar">
-      <Grid container item xs={10} justify="space-around" alignItems="center">
-        <Grid item xs={2}>
+      <Grid container item xs={11} md={10} justify="space-between" alignItems="center">
+        <Hidden mdUp>
+          <Grid item xs={2}>
+            <MenuIcon style={{ fontSize: 30 }} />
+          </Grid>
+        </Hidden>
+        <Grid item xs={3}>
           <Link to="/" replace>
             <img src={CorebizLogo} alt="Corebiz Logo" className="navbar-logo" />
           </Link>
         </Grid>
-        <Grid item container xs={6} lg={7}>
-          <Debounce time="1000" handler="onChange">
-            <Input
-              placeholder="O que está procurando?"
-              fullWidth
-              onChange={searchContent}
-              endAdornment={(
-                <InputAdornment position="end">
-                  <SearchIcon />
-                </InputAdornment>
-              )}
-            />
-          </Debounce>
-        </Grid>
-        <Hidden mdDown>
-          <Grid item xs={2} lg={1}>
-            <Button startIcon={<PersonIcon />} className="my-account">Minha Conta</Button>
+        <Hidden smDown>
+          <Grid item container md={5}>
+            <SearchInput onChange={searchContent} />
           </Grid>
         </Hidden>
-        <Grid item xs={1}>
-          <Badge badgeContent={cartStore?.products?.length} color="secondary">
-            <CartIcon />
-          </Badge>
+        <Grid container item xs={2} md={4} lg={4} alignItems="center" justify="flex-end">
+          <Hidden smDown>
+            <Grid container item xs={6} justify="flex-end">
+              <Button startIcon={<PersonIcon />} className="my-account">Minha Conta</Button>
+            </Grid>
+          </Hidden>
+          <Grid container item xs={12} md={2} justify="flex-end">
+            <Badge badgeContent={cartStore?.products?.length} color="secondary">
+              <CartIcon />
+            </Badge>
+          </Grid>
         </Grid>
       </Grid>
+      <Hidden mdUp>
+        <Grid container item xs={11} justify="center">
+          <SearchInput onChange={searchContent} />
+        </Grid>
+      </Hidden>
     </Grid>
   );
 };
